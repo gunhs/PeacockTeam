14c77c0e6c644e85a5bfc45b1d36bf499be0b568
gunhs <mr-vaska@yandex.ru>
2023-11-14 14:32:34 +0300

    1G construct completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,124 @@
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.*;
 
 public class Main {
+    private static List<Map<String, Integer>> positionWordLine;
+    private static int[] linesGroups;
+    private static List<List<Integer>> groups;
 
     public static void main(String[] args) throws IOException {
         long start = System.currentTimeMillis();
-//        if (args.length == 0) {
-//            System.out.println("Please provide a file name as an argument.");
-//            return;
-//        }
-//
-//        String fileName = args[0];
-//        Path pathSrc = Paths.get(fileName);
-        Path pathSrc = Paths.get("lng-big.csv");
+        if (args.length == 0) {
+            System.out.println("Please provide a file name as an argument.");
+            return;
+        }
+
+        String fileName = args[0];
+        Path pathSrc = Paths.get(fileName);
+//        Path pathSrc = Paths.get("lng-big.csv");
 //        Path pathSrc = Paths.get("lng.txt");
         Path pathDst = Paths.get("out.txt");
-        NumberSeparator numberSeparator = new NumberSeparator();
-        numberSeparator.numberSeparate(pathDst, pathSrc);
+        numberSeparate(pathDst, pathSrc);
         System.out.println(((System.currentTimeMillis() - start) / 1000) + " sec");
+    }
+
+    public static void numberSeparate(Path pathDst, Path pathSrc) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(pathSrc.toFile()))) {
+            int countSize = loadFile(pathSrc).size();
+            int numberCurrentString = -1;
+            linesGroups = new int[countSize];
+            groups = new ArrayList<>();
+            positionWordLine = new ArrayList<>();
+            String currentString;
+            while ((currentString = reader.readLine()) != null) {
+                numberCurrentString++;
+                String[] words = currentString.split(";");
+                lineHandler(words, numberCurrentString);
+            }
+            linesGroups = null;
+            positionWordLine.clear();
+            writeFile(pathSrc, pathDst);
+        }
+    }
+
+    private static void lineHandler(String[] words, int numberCurrentString) {
+        linesGroups[numberCurrentString] = -1;
+        for (int j = 0; j < words.length; j++) {
+            String currentWord = words[j];
+            wordHandler(numberCurrentString, currentWord, j);
+        }
+    }
+
+    private static void wordHandler(int numberCurrentString, String currentWord, int columnNumber) {
+        if (positionWordLine.size() == columnNumber) {
+            positionWordLine.add(new HashMap<>());
+        }
+        if (currentWord.equals("\"\"") || currentWord.isEmpty()) {
+            return;
+        }
+        Map<String, Integer> wordMap = positionWordLine.get(columnNumber);
+        if (wordMap.get(currentWord) != null) {
+            int existingLine = wordMap.get(currentWord);
+            int existingGroupNumber = linesGroups[existingLine];
+            int currentGroupNumber = linesGroups[numberCurrentString];
+            if (currentGroupNumber != -1) {
+                if (existingGroupNumber == currentGroupNumber) {
+                    return;
+                }
+                if (existingGroupNumber != -1) {
+                    List<Integer> currentGroup = groups.get(currentGroupNumber);
+                    groups.get(existingGroupNumber).addAll(currentGroup);
+                    for (Integer l : groups.get(currentGroupNumber)) {
+                        linesGroups[l] = existingGroupNumber;
+                    }
+                    groups.set(currentGroupNumber, null);
+                } else {
+                    groups.get(currentGroupNumber).add(existingLine);
+                    linesGroups[existingLine] = currentGroupNumber;
+                }
+            } else if (existingGroupNumber != -1) {
+                groups.get(existingGroupNumber).add(numberCurrentString);
+                linesGroups[numberCurrentString] = existingGroupNumber;
+            } else {
+                List<Integer> linesInGroup = new ArrayList<>();
+                linesInGroup.add(existingLine);
+                linesInGroup.add(numberCurrentString);
+                groups.add(linesInGroup);
+                int groupNumber = groups.size() - 1;
+                linesGroups[existingLine] = groupNumber;
+                linesGroups[numberCurrentString] = groupNumber;
+            }
+        }
+        positionWordLine.get(columnNumber).put(currentWord, numberCurrentString);
+    }
+
+
+    private static void writeFile(Path pathSrc, Path pathDst) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(pathDst.toFile()))) {
+            int countGroups = 0;
+            List<String> lines = loadFile(pathSrc);
+            for (List<Integer> group : groups) {
+                if (group == null) {
+                    continue;
+                }
+                Set<String> linesInGroup = new LinkedHashSet<>();
+                for (int line : group) {
+                    linesInGroup.add(lines.get(line));
+                }
+                countGroups++;
+                writer.write("Группа " + countGroups + "\n");
+                for (String l : linesInGroup) {
+                    writer.write(l + "\n");
+                }
+            }
+            writer.write("\n" + "Количество групп: " + countGroups + "\n");
+        }
+    }
+
+    public static List<String> loadFile(Path path) throws IOException {
+        return Files.readAllLines(path);
     }
 }
--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -9,16 +9,16 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 
 public class TestMain {
+
     @Test
     @DisplayName("3 строчки с повторениями")
     void testThreeLineMatch() throws IOException {
-        NumberSeparator numberSeparator = new NumberSeparator();
         Path path = Paths.get("data/tests/testThreeLineMatchResult.txt");
         Path dstPath = Paths.get("data/tests/testThreeLineMatchDst.txt");
         Path srcPath = Paths.get("data/tests/testThreeLineMatchSrc.txt");
-        List<String> expectedLines = numberSeparator.loadFile(dstPath);
-        numberSeparator.numberSeparate(path, srcPath);
-        List<String> actualLines = numberSeparator.loadFile(path);
+        List<String> expectedLines = Main.loadFile(dstPath);
+        Main.numberSeparate(path, srcPath);
+        List<String> actualLines = Main.loadFile(path);
         System.out.println(dstPath.toAbsolutePath());
         System.out.println(path.toAbsolutePath());
         assertIterableEquals(expectedLines, actualLines);
@@ -27,13 +27,12 @@ public class TestMain {
     @Test
     @DisplayName("3 строчки без повторения")
     void testThreeLineNonMatch() throws IOException {
-        NumberSeparator numberSeparator = new NumberSeparator();
         Path path = Paths.get("data/tests/testThreeLineNonMatchResult.txt");
         Path dstPath = Paths.get("data/tests/testThreeLineNonMatchDst.txt");
         Path srcPath = Paths.get("data/tests/testThreeLineNonMatchSrc.txt");
-        List<String> expectedLines = numberSeparator.loadFile(dstPath);
-        numberSeparator.numberSeparate(path, srcPath);
-        List<String> actualLines = numberSeparator.loadFile(path);
+        List<String> expectedLines = Main.loadFile(dstPath);
+        Main.numberSeparate(path, srcPath);
+        List<String> actualLines = Main.loadFile(path);
         System.out.println(dstPath.toAbsolutePath());
         System.out.println(path.toAbsolutePath());
         assertIterableEquals(expectedLines, actualLines);
@@ -42,13 +41,12 @@ public class TestMain {
     @Test
     @DisplayName("3 строчки c повторениями и без")
     void testThreeLineHalfMatch() throws IOException {
-        NumberSeparator numberSeparator = new NumberSeparator();
         Path path = Paths.get("data/tests/testThreeLineHalfMatchResult.txt");
         Path dstPath = Paths.get("data/tests/testThreeLineHalfMatchDst.txt");
         Path srcPath = Paths.get("data/tests/testThreeLineHalfMatchSrc.txt");
-        List<String> expectedLines = numberSeparator.loadFile(dstPath);
-        numberSeparator.numberSeparate(path, srcPath);
-        List<String> actualLines = numberSeparator.loadFile(path);
+        List<String> expectedLines = Main.loadFile(dstPath);
+        Main.numberSeparate(path, srcPath);
+        List<String> actualLines = Main.loadFile(path);
         System.out.println(dstPath.toAbsolutePath());
         System.out.println(path.toAbsolutePath());
         assertIterableEquals(expectedLines, actualLines);
@@ -57,13 +55,12 @@ public class TestMain {
     @Test
     @DisplayName("4 строчки проверка дубликтов")
     void testThreeLineDuplicateLines() throws IOException {
-        NumberSeparator numberSeparator = new NumberSeparator();
         Path path = Paths.get("data/tests/testThreeLineDuplicateResult.txt");
         Path dstPath = Paths.get("data/tests/testThreeLineDuplicateDst.txt");
         Path srcPath = Paths.get("data/tests/testThreeLineDuplicateSrc.txt");
-        List<String> expectedLines = numberSeparator.loadFile(dstPath);
-        numberSeparator.numberSeparate(path, srcPath);
-        List<String> actualLines = numberSeparator.loadFile(path);
+        List<String> expectedLines = Main.loadFile(dstPath);
+        Main.numberSeparate(path, srcPath);
+        List<String> actualLines = Main.loadFile(path);
         System.out.println(dstPath.toAbsolutePath());
         System.out.println(path.toAbsolutePath());
         assertIterableEquals(expectedLines, actualLines);
@@ -72,13 +69,12 @@ public class TestMain {
     @Test
     @DisplayName("2 строчки проверка дубликтов")
     void testTwoLineDuplicateLines() throws IOException {
-        NumberSeparator numberSeparator = new NumberSeparator();
         Path path = Paths.get("data/tests/testTwoLineDuplicateResult.txt");
         Path dstPath = Paths.get("data/tests/testTwoLineDuplicateDst.txt");
         Path srcPath = Paths.get("data/tests/testTwoLineDuplicateSrc.txt");
-        List<String> expectedLines = numberSeparator.loadFile(dstPath);
-        numberSeparator.numberSeparate(path, srcPath);
-        List<String> actualLines = numberSeparator.loadFile(path);
+        List<String> expectedLines = Main.loadFile(dstPath);
+        Main.numberSeparate(path, srcPath);
+        List<String> actualLines = Main.loadFile(path);
         System.out.println(dstPath.toAbsolutePath());
         System.out.println(path.toAbsolutePath());
         assertIterableEquals(expectedLines, actualLines);
@@ -87,13 +83,12 @@ public class TestMain {
     @Test
     @DisplayName("5 строчек. 3 и 2 совпадения. Две группы")
     void testFiveLineTwoGroup() throws IOException {
-        NumberSeparator numberSeparator = new NumberSeparator();
         Path path = Paths.get("data/tests/testFiveLineTwoGroupResult.txt");
         Path dstPath = Paths.get("data/tests/testFiveLineTwoGroupDst.txt");
         Path srcPath = Paths.get("data/tests/testFiveLineTwoGroupSrc.txt");
-        List<String> expectedLines = numberSeparator.loadFile(dstPath);
-        numberSeparator.numberSeparate(path, srcPath);
-        List<String> actualLines = numberSeparator.loadFile(path);
+        List<String> expectedLines = Main.loadFile(dstPath);
+        Main.numberSeparate(path, srcPath);
+        List<String> actualLines = Main.loadFile(path);
         System.out.println(dstPath.toAbsolutePath());
         System.out.println(path.toAbsolutePath());
         assertIterableEquals(expectedLines, actualLines);
@@ -102,13 +97,12 @@ public class TestMain {
     @Test
     @DisplayName("5 строчек. 4 и 2 совпадения. Одна группа")
     void testFiveLineOneGroup() throws IOException {
-        NumberSeparator numberSeparator = new NumberSeparator();
         Path path = Paths.get("data/tests/testFiveLineOneGroupResult.txt");
         Path dstPath = Paths.get("data/tests/testFiveLineOneGroupDst.txt");
         Path srcPath = Paths.get("data/tests/testFiveLineOneGroupSrc.txt");
-        List<String> expectedLines = numberSeparator.loadFile(dstPath);
-        numberSeparator.numberSeparate(path, srcPath);
-        List<String> actualLines = numberSeparator.loadFile(path);
+        List<String> expectedLines = Main.loadFile(dstPath);
+        Main.numberSeparate(path, srcPath);
+        List<String> actualLines = Main.loadFile(path);
         System.out.println(dstPath.toAbsolutePath());
         System.out.println(path.toAbsolutePath());
         assertIterableEquals(expectedLines, actualLines);
